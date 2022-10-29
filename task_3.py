754aba5766788247a509e8e2a68e3c92dbdaf0eb
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-29 22:37:43 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,6 +1,34 @@
 def is_prime(n):
-    return
+    dzielniki = []
+    i = 2
+    flag = 0
+
+    while i != n:
+
+        if i not in dzielniki:
+
+            if n % i != 0:
+
+                for j in range(i, n, i):
+                    dzielniki.append(j)
+
+            else:
+                flag = 1
+                break
+
+        i += 1
+
+    if flag == 1:
+        return False
+
+    else:
+        return True
+
 
 
 def is_diabolic(n):
-    return
+    if "666" in str(n):
+        return True
+
+    else:
+        return False
