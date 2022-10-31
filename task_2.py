1d1132843266b8cdf674f81a16f65eb8eb31f704
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-31 21:29:01 +0000

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -2,24 +2,25 @@ import turtle
 import random
 
 def square(height, length, space):
-  
-  turtle.left(90)
-  turtle.forward(height)
-  turtle.right(90)
-  turtle.forward(length)
-  turtle.right(90)
-  turtle.forward(height)
-  turtle.right(90)
-  turtle.forward(length)
-  turtle.right(180)
-  turtle.forward(length + space)
-  
-height = 90
-length = 30
-space = 20
+    turtle.left(90)
+    turtle.forward(height)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.forward(height)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(180)
+    turtle.forward(length + space)
+
+
+height = 27
+length = 9
+space = 6
 
 for i in range(20):
-  
-  square(height, length, space)
-  
-  height = height + random.random()
+    square(height, length, space)
+
+    height = height + 10 * random.random()
+
+turtle.exitonclick()
